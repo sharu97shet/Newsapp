ef62f465c21f3d4239099f37397bb8addd2c3739
sharu97shet <sharathshet.n@gmail.com>
2023-10-24 17:05:11 +0530

    first commit

---
+++ Newsapi/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+   
+    path('', views.home, name='index'),
+    path('homepage', views.homepage,  name='index')
+  
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ Newsapi/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, HttpResponse , redirect
+import http
+import requests
+API_KEY = '8f70a9bce48c4a8d93552d50d58ecbd9'
+
+
+# Create your views here.
+
+def home(request):
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        print(data)
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+
+
+
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'Newspage.html', context)
+
+def homepage(request):
+
+  #return render(request, 'Newspage.html')
+  return HttpResponse("work")
