083768236401443b7c8c8d9f368814d3d08a0621
Sharath N Shet <111754387+sharu97shet@users.noreply.github.com>
2023-12-19 23:43:44 +0530

    Update settings.py

--- DjangoNewsProject/settings.py
+++ DjangoNewsProject/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Enable WhiteNoise to serve static files
 STATIC_URL = '/static/'
 STATIC_ROOT= 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
